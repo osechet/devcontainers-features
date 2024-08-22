151ceb42aa56d8f958c36b30108e94c98117228a
Olivier SECHET <osechet@gmail.com>
2024-08-22 16:56:48 +0200

    feat(goinstall)!: update goinstall to install multiple packages

--- src/goinstall/install.sh
+++ src/goinstall/install.sh
@@ -2,43 +2,18 @@
 
 set -x
 
-PACKAGE="${PACKAGE:-""}"
-VERSION="${VERSION:-"latest"}"
+PACKAGES="${PACKAGES:-""}"
 
-USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
-
-if [ -z "${PACKAGE}" ]; then
-  echo "Missing package"
+if [ -z "${PACKAGES}" ]; then
+  echo "Missing packages"
   exit 1
 fi
 
-sudo_if() {
-    COMMAND="$*"
-    if [ "$(id -u)" -eq 0 ] && [ "$USERNAME" != "root" ]; then
-        su - "$USERNAME" -c "$COMMAND"
-    else
-        $COMMAND
-    fi
-}
+IFS=',' read -r -a packages <<< "$PACKAGES"
 
-# Determine the appropriate non-root user
-if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-    USERNAME=""
-    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-            USERNAME=${CURRENT_USER}
-            break
-        fi
-    done
-    if [ "${USERNAME}" = "" ]; then
-        USERNAME=root
-    fi
-elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
-    USERNAME=root
-fi
-
-sudo_if go install ${PACKAGE}@${VERSION}
+for package in ${packages[@]}; do
+  go install ${package}
+done
 
 
 echo "Done!"
